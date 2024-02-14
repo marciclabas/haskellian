9f612b1c0e350ae18cad7c070bffbd078b2c6335
m4rs <marcel@marcelclaramunt.cat>
2024-02-14 06:11:25 +0100

    newver

--- src/haskellian/asynch/__init__.py
+++ src/haskellian/asynch/__init__.py
@@ -1,4 +1,4 @@
 from .asynch import either, uneither, safe, wrap, then, bind, wait
 from .iterables import map, enumerate, flatten, asyncify, syncify, skip, take, split
 from .memo import AsyncMemo
-from .queue import Guard, Queue, iterate
+from .queue import Guard, Queue, iterate, enqueue, pop_all
--- src/haskellian/asynch/queue.py
+++ src/haskellian/asynch/queue.py
@@ -1,6 +1,6 @@
 from typing import TypeVar, AsyncIterable
 import asyncio
-
+from .. import either
 
 class Guard: ...
 A = TypeVar('A')
@@ -12,3 +12,21 @@ async def iterate(q: asyncio.Queue[A | Guard]) -> AsyncIterable[A]:
         match await q.get():
             case Guard(): return
             case x: yield x
+            
+def enqueue(xs: AsyncIterable[A], guard: bool = False) -> asyncio.Queue[A]:
+    """Enqueue `xs` into the returned queue. Optionally, add `Guard` at the end"""
+    q = asyncio.Queue()
+    async def f():
+        async for x in xs:
+            q.put_nowait(x)
+        if guard:
+            q.put_nowait(Guard())
+    asyncio.Task(f())
+    return q
+    
+def pop_all(q: asyncio.Queue[A]) -> list[A]:
+    """Synchronously return all elements of the queue"""
+    xs = []
+    while not isinstance((x := either(q.get_nowait)), asyncio.QueueEmpty):
+        xs += [x]
+    return xs
